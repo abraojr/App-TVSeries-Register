652047bad8a3ca8cf425d3347d085e20d09db2e2
Abrão Astério Junior <abcorinthians@gmail.com>
2021-07-23 04:54:23 -0300

    Implementing IRepository and TVShowRepository

---
+++ app-Series-Register/Entities/TVShowRepository.cs
@@ -0,0 +1,40 @@
+﻿using app_Series_Register.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace app_Series_Register
+{
+    class TVShowRepository : IRepository<TVShow>
+    {
+        private List<TVShow> listSeries = new List<TVShow>();
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TVShow GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(TVShow entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TVShow> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int nextId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, TVShow entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ app-Series-Register/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace app_Series_Register.Interfaces
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+        T GetById(int id);
+        void Insert(T entity);
+        void Delete(int id);
+        void Update(int id, T entity);
+        int nextId();
+    }
+}
